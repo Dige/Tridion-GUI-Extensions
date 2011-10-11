5ad0aed8e786f8e766ef00ea19e2b997631790de
Vesa Paakkanen <vesapaakkanen@gmail.com>
2011-10-11 11:48:04 +0200

    moved the prototypes outside the class

--- ReplacePreview/client/commands/ViewInFrontEnd.js
+++ ReplacePreview/client/commands/ViewInFrontEnd.js
@@ -7,45 +7,46 @@ function ViewInFrontEnd(settings)
 {
 	Type.enableInterface(this, settings.fullQName);
 	this.addInterface("Tridion.Cme.Command", [settings.className]);
+    this.settings = settings;
+};
 
-    this.prototype._isAvailable = function (selection, pipeline)
-    {
-        var items = selection.getItems();
+ViewInFrontEnd.prototype._isAvailable = function (selection, pipeline)
+{
+    var items = selection.getItems();
 
-        if(items.length > 1) {
+    if(items.length > 1) {
+        return false;
+    }
+    var itemId = selection.getItem(0);
+    var item = $models.getItem(itemId);
+
+    if(item){
+        if(item.getItemType() != $const.ItemType.PAGE){
             return false;
         }
-        var itemId = selection.getItem(0);
-        var item = $models.getItem(itemId);
+    }
 
-        if(item){
-            if(item.getItemType() != $const.ItemType.PAGE){
-                return false;
-            }
-        }
-
-        if (pipeline) {
-            pipeline.stop = false;
-        }
-        return true;
-    };
+    if (pipeline) {
+        pipeline.stop = false;
+    }
+    return true;
+};
 
 
-    this.prototype._isEnabled = function(selection, pipeline)
-    {
-        if (pipeline) {
-            pipeline.stop = false;
-        }
-        return this._isAvailable(selection, pipeline);
-    };
+ViewInFrontEnd.prototype._isEnabled = function(selection, pipeline)
+{
+    if (pipeline) {
+        pipeline.stop = false;
+    }
+    return this._isAvailable(selection, pipeline);
+};
 
 
-    this.prototype._execute = function (selection, pipeline)
-    {
-        if (pipeline) {
-            pipeline.stop = false;
-        }
-    };
+ViewInFrontEnd.prototype._execute = function (selection, pipeline)
+{
+    if (pipeline) {
+        pipeline.stop = false;
+    }
 };
 
 
